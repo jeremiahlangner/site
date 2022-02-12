9302f9ce4b3b20e5f8ecaa563f75da6abb46243f
Jeremiah Langner <jlangner@gsu.edu>
2022-02-12 13:37:00 -0700

    update resume

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,38 @@
 /*!
 * Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+(function(){
+    let stop = {};
+
+    const downArrow = document.querySelector('.down_arrow');
+    document.addEventListener('click', e => {
+        if (e.srcElement && e.srcElement == downArrow) {
+            try {
+                scrollNext();
+            } catch (e) {
+                if (e !== stop) throw e;
+            }
+        }
+    });
+    
+    function scrollNext() {
+        const sections = document.querySelectorAll('.show-on-scroll');
+        [].forEach.call(sections, (section) => {         
+            if (offset(section) > window.scrollY + window.innerHeight / 2) {
+                window.scrollTo(0, offset(section) - window.innerHeight / 3 );
+                throw stop;
+            }
+        });
+        window.scrollTo(0, 0);
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return rect.top + scrollTop
+    }
+})();
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,33 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+(function(){
+    let stop = {};
+
+    const downArrow = document.querySelector('.down_arrow');
+    document.addEventListener('click', e => {
+        if (e.srcElement && e.srcElement == downArrow) {
+            try {
+                scrollNext();
+            } catch (e) {
+                if (e !== stop) throw e;
+            }
+        }
+    });
+    
+    function scrollNext() {
+        const sections = document.querySelectorAll('.show-on-scroll');
+        [].forEach.call(sections, (section) => {         
+            if (offset(section) > window.scrollY + window.innerHeight / 2) {
+                window.scrollTo(0, offset(section) - window.innerHeight / 3 );
+                throw stop;
+            }
+        });
+        window.scrollTo(0, 0);
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return rect.top + scrollTop
+    }
+})();
